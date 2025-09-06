597730f8f6f23977410d94e51e883c5344ab8a12
Zane George <146121353+Zane-Dev14@users.noreply.github.com>
2025-09-06 20:03:10 +0530

    That hook didnt work, forcing it to run last.

--- archlive/airootfs/root/customize_airootfs.sh
+++ archlive/airootfs/root/customize_airootfs.sh
@@ -5,3 +5,21 @@ echo "NeuronOS" > /etc/hostname
 systemctl enable NetworkManager.service
 systemctl enable gdm.service
 systemctl enable docker.service
+
+
+KVER="6.16.4-2-mits-capstone-Mits-Capstone"
+
+# Copy kernel and generate initramfs
+if [ -f "/lib/modules/$KVER/vmlinuz" ]; then
+    echo "Copying kernel..."
+    cp /lib/modules/$KVER/vmlinuz /boot/vmlinuz-linux-mits-capstone
+
+    echo "Generating initramfs..."
+    mkinitcpio -k "$KVER" -g "/boot/initramfs-linux-mits-capstone.img"
+    mkinitcpio -k "$KVER" -g "/boot/initramfs-linux-mits-capstone-fallback.img" -S autodetect
+
+    echo "Kernel + initramfs ready"
+else
+    echo "ERROR: Kernel not found at /lib/modules/$KVER/vmlinuz"
+    exit 1
+fi
